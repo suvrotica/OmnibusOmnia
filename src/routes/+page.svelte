bfb6545d2fd15743d0470b369015371406a08fce
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-14 05:00:08 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <Title stringA="The" stringB="Name" />
 
 <p>
-	I couldn't just flippantly say a scratchpad of thoughts, extension of my gorilla head, unrelated
+	I couldn't just flippantly say a scratchpad of thoughts, extension of my absuredly oversized head, unrelated
 	figments strewn around, collectibles from the waste basket, or, more crudely, everything about
 	anything. But that's exactly what I always wanted my blog to be. I'm not a prisoner to the usual
 	boundaries that limit most people, and I wanted a place where people like me could find a way to
