16c51cf42223089a8685dcc56267b1ce85b76dfd
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-14 05:42:07 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,9 +12,9 @@
 	read my thoughts and not feel alone.
 </p>
 <p>
-	In Latin, an "omnibus" is a volume containing several novels or other items previously published separately. This usage likely comes from the idea of bringing together many works "for all" to enjoy. "Omnibus" is a dative plural form of "omnis," meaning "all" or "every." In this context,
-	It implies "for all." "Omnia" is the nominative or accusative neuter plural form of "omnis."
-	meaning "all things" or "everything." Thus, "Omnibus Omnia" translates to "For Everyone, About
+	In Latin, an "omnibus" is a volume containing several novels or other items previously published separately. 
+This usage likely comes from the idea of bringing together many works "for all" to enjoy. 
+"Omnibus" thus implies "for all." <p>"Omnia" means "all things" or "everything."</p> <p>Thus, "Omnibus Omnia" translates to "For Everyone, About
 	Anything." And that satisfies the nerd inside me. The phrase expresses the idea of inclusiveness or universal provision. A pompous name for something that could have
 	otherwise been ignored as a trifle.
 </p>
