25b21adab61cf5b3458b86287edd2c82f9399194
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-15 23:42:09 +0530

    Create Blog-001.svelte

---
+++ src/routes/blog/Blog-001.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+<div class="grid">
+
+  <div>1</div>
+
+  <p>You must know, you can't be slow,
+Got that huge ass horn to toot. 
+Otherwise they think you're not,
+Even worth a fucking smelly poot.
+
+Nobody really cares what you know,
+And by now you ought to see.
+The race of men want their ass licked,
+And you get paid for a slave to be.
+
+It's just this species that has it's head,
+Right and tight and way inside its ass.
+Ants and many others cooperate,
+Swarm intelligence is another class.
+
+No problem with prudes, can't call them rude,
+The language we don't really know.
+Euphemisms and superlative accolades,
+We're sissy slaves with balls that need to grow. </p>
+
+  <div>3</div>
+
+  <div>4</div>
+
+</div>
