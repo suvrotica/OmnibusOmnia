89bcb489ea30de5dead657a94d485662a334f08d
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-16 02:24:30 +0530

    Update Blog-001.svelte

--- src/routes/blog/Blog-001.svelte
+++ src/routes/blog/Blog-001.svelte
@@ -4,32 +4,35 @@
 
 <div class="grid">
 
-  <div>
-<img src="" />
-</div>
+  <article>
 
-  <p>You must know, you can't be slow,
+<img src="svg_horse-dick.svg" alt="horse" />
+</article>
+
+  <article>You must know, you can't be slow,
 Got that huge ass horn to toot. 
 Otherwise they think you're not,
 Even worth a fucking smelly poot.
-
+<br><br>
 Nobody really cares what you know,
 And by now you ought to see.
 The race of men want their ass licked,
 And you get paid for a slave to be.
-
+<br><br>
 It's just this species that has it's head,
 Right and tight and way inside its ass.
 Ants and many others cooperate,
-Swarm intelligence is another class.
+Swarm intelligence is another class.<br><br>
 
 No problem with prudes, can't call them rude,
 The language we don't really know.
 Euphemisms and superlative accolades,
-We're sissy slaves with balls that need to grow. </p>
+We're sissy slaves with balls that need to grow. 
+<br><br>
+</article>
 
-  <div>3</div>
+  
 
-  <div>4</div>
+  
 
 </div>
