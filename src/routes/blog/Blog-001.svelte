b3940f1221ab5d7c3fe88de24ec48ea788ee5129
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-16 02:36:52 +0530

    Update Blog-001.svelte

--- src/routes/blog/Blog-001.svelte
+++ src/routes/blog/Blog-001.svelte
@@ -4,31 +4,31 @@
 
 <div class="grid">
 
-  <article>
+  <article  class="svg" >
 
-<img class="svg" src="toota164883d.svg" alt="horse" />
+<img  src="toota164883d.svg" alt="horse" />
 </article>
 
   <article>You must know, you can't be slow,
-Got that huge ass horn to toot. 
-Otherwise they think you're not,
+<br>Got that huge ass horn to toot. <br>
+Otherwise they think you're not,<br>
 Even worth a fucking smelly poot.
 <br><br>
-Nobody really cares what you know,
-And by now you ought to see.
-The race of men want their ass licked,
+Nobody really cares what you know,<br>
+And by now you ought to see.<br>
+The race of men want their ass licked,<br>
 And you get paid for a slave to be.
 <br><br>
-It's just this species that has it's head,
-Right and tight and way inside its ass.
-Ants and many others cooperate,
+It's just this species that has it's head,<br>
+Right and tight and way inside its ass.<br>
+Ants and many others cooperate,<br>
 Swarm intelligence is another class.<br><br>
 
-No problem with prudes, can't call them rude,
-The language we don't really know.
-Euphemisms and superlative accolades,
+No problem with prudes, can't call them rude,<br>
+The language we don't really know.<br>
+Euphemisms and superlative accolades,<br>
 We're sissy slaves with balls that need to grow. 
-<br><br>
+
 </article>
 
   
