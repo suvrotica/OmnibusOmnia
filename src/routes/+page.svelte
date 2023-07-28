8e87e90dd791f14af04663eb676f3ad25c214c13
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-28 08:51:00 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,6 +53,14 @@ These remind people of god, but remind me that I'm always full of shit, and unle
 
 <details>
 	<summary>Blogs</summary>
+<B4
+		blogContent={`Title:United we stand 
+Date:2023-jul-28
+42br
+
+`}
+		image_name="mons.svg"
+	/>
 	<B4
 		blogContent={`Title:Shake Shake Shake
 Date:2023-jul-28
