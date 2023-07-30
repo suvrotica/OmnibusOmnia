50557237c2be4a96dcd660650f1db96ef0b9c3fa
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-07-30 14:38:01 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,7 +53,13 @@ These remind people of god, but remind me that I'm always full of shit, and unle
 
 <details>
 	<summary>Blogs</summary>
-
+<B4
+		blogContent={`Title:Lemons dick
+	Date:2023-jul-30
+	42br
+`} 
+image_name ="dickf.svg"
+/>
 <B4 blogContent ={`
 Title:Make america great again 
 Date:2023-jul-30
