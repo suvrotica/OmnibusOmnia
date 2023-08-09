048cd31bb9f67cf6402df0c47c12d58cf73df997
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-08-09 12:40:00 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 <details>
 	<summary>Welcome Posts</summary>
 	<B4
-		blogContent={`Title:Memoir
+		blogContent={`Title:Memoir? 
 	Date:2023-aug-9
 	42br
 I'm intent on keeping a memoir because I know that when I'm gone, there will be people wondering what music I liked, what books I read, and what defined this ludicrous audacity of a bengali black sheep, and I don't want to disappoint them.
