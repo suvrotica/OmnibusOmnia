3ee5e47d39f4ae4d0cd328628de5f6da50331c94
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-08-14 21:05:56 +0530

    Update utility-functions.ts

--- src/lib/utility-functions.ts
+++ src/lib/utility-functions.ts
@@ -65,12 +65,11 @@ export function replace42picWithFigure(blogContent: string): string {
 	});
 }
 
-export function replace42yt(text: string): string {
-  const pattern = /42yt\((.*?)\)/g;
-  const replacement = `<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-
-  return text.replace(pattern, replacement);
+export function replace42yt(blogContent: string): string {
+	return blogContent.replace(/42yt\((.*?)\)/g, (match, imageName) => {
+		return `<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;});
 }
+
 
 
 
