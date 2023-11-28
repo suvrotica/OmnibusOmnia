11dc1a4ae2c9723c3132a2cca66c2d51f0b6d3a4
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-11-28 06:02:48 +0530

    Add B4 component to blog page

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let data;
 	console.log(data);
+	import B4 from '../Blog-004.svelte';
 </script>
 
 <h1>Blog Posts</h1>
@@ -9,8 +10,9 @@
 		{#each data?.blogPosts as post}
 			<div class="blog-post">
 				<h2>{post.title}</h2>
-				<img src={post.image_url} alt={post.title} />
-				<p>{post.content}</p>
+				<!-- <img src={post.image_url} alt={post.title} /> -->
+				<!-- <p>{post.content}</p> -->
+				<B4 blogContent={post.content} image_name={post.image_url} />
 			</div>
 		{/each}
 	</div>
