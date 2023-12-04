f2f0fb6a3c40bcdf05079901f21cf2febdea8bbc
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 10:59:30 +0530

    Update post.svelte

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -35,14 +35,14 @@
 <details>
 	<!-- svelte-ignore a11y-no-redundant-roles -->
 	<summary role="button" class="secondary"
-		><span><img src={post.image_url} width=50px alt="pic" /></span>{'   ' +
+		><span><img src={post.image_url} width=50px alt="pic" />{'   ' +
 			post.created_at.toLocaleDateString('en-GB', {
 				year: '2-digit',
 				month: 'short',
 				day: '2-digit'
 			}) +
 			'-' +
-			post.title}</summary
+			post.title}</span></summary
 	>
 
 	<article class="content">
