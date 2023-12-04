a80d96e0e8984a3882a7e11157c2495f3b98b1f0
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 11:15:16 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,8 +43,8 @@
 	}
 	:global(summary) {
 		font-family: 'Gaegu', cursive;
-		font-weight: 700;
-		font-size: 32px;
+		font-weight: 300;
+		font-size: 24px;
 	}
 	:global(h1, h2, h3, h4, h5, h6) {
 		font-family: 'Amatic SC', cursive;
