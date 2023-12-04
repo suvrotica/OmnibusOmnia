bdbbfc7587dc1c8adaa2e649682f8827f95b1b47
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 09:20:49 +0530

    Update +page.svelte

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,12 +1,29 @@
 <script>
-	export let data;
-	import Post from '$lib/components/post.svelte';
+    export let data;
+    import Post from '$lib/components/post.svelte';
 </script>
 
+<style>
+    .gallery {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        grid-gap: 16px;
+        padding: 16px;
+    }
+
+    .no-posts {
+        text-align: center;
+        font-size: 1.2em;
+        margin-top: 20px;
+    }
+</style>
+
 {#if data.blogPosts && data.blogPosts.length > 0}
-	{#each data.blogPosts as post}
-		<Post {post} />
-	{/each}
+    <div class="gallery">
+        {#each data.blogPosts as post}
+            <Post {post} />
+        {/each}
+    </div>
 {:else}
-	<h2>No blog posts available</h2>
+    <h2 class="no-posts">No blog posts available</h2>
 {/if}
