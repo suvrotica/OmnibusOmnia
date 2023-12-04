a89ccea56c852a1a5dabb6bb9ac9ac5818d0e0c3
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 12:18:59 +0530

    Update +page.svelte

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -3,9 +3,21 @@
 	import Post from '$lib/components/post.svelte';
 </script>
 
+
+<details open>
+  
 {#if data.blogPosts && data.blogPosts.length > 0}
 	{#each data.blogPosts as post}
-		<Post {post} />
+           <details>
+               <summary>Music</summary>
+               {#if post.tag_set[0]==="Technical"}
+                  <Post {post} />
+               {/if}
+           </details>
+           <details>
+               <summary>all</summary>
+               <Post {post} />
+           </details>
 	{/each}
 {:else}
 	<h2>No blog posts available</h2>
