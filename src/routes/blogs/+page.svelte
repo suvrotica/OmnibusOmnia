1cf688b0e2ba3642b461ae146aea43d006686f12
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 12:37:07 +0530

    Update +page.svelte

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -7,22 +7,16 @@
 
   
 {#if data.blogPosts && data.blogPosts.length > 0}
-	{#each data.blogPosts as post}
-           <details>
-               
-               {#if post.tag_set==="Music"}
-                  <summary>Music</summary>
-                  <Post {post} />
-               {:else}
-                  <summary>Blogs</summary>
-                  <Post {post} />
-                {/if}
-           </details>
-           <details>
-               <summary>all</summary>
-               <Post {post} />
-           </details>
-	{/each}
+    {#each data.blogPosts as post}
+        <details>
+            <summary>{post.tag_set === "Music" ? "Music" : "Blogs"}</summary>
+            <Post {post} />
+        </details>
+        <details>
+            <summary>All</summary>
+            <Post {post} />
+        </details>
+    {/each}
 {:else}
-	<h2>No blog posts available</h2>
+    <h2>No blog posts available</h2>
 {/if}
