2562294f1199b39317d14d43183768a8299c3580
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 10:37:55 +0530

    Update +page.svelte

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,62 +1,12 @@
 <script>
-    import Post from '$lib/components/post.svelte';
-
-    export let data;
-    let selectedID;
-
-    function selectPost(id) {
-        selectedID = id;
-    }
+	export let data;
+	import Post from '$lib/components/post.svelte';
 </script>
 
-<style>
-    .item1 { grid-area: header; }
-    .item2 { grid-area: menu; }
-    .item3 { grid-area: main; }
-
-    .grid-container {
-        display: grid;
-        grid-template-areas:
-        'header header header header header header'
-        'menu main main main main main';
-        gap: 10px;
-        
-        padding: 10px;
-    }
-
-    .grid-container > div {
-        
-        text-align: center;
-        padding: 10px 0;
-        font-size: 20px;
-    }
-</style>
-
-<h1>Blog Layout</h1>
-
-<div class="grid-container">
-    <div class="item1">Header</div>
-    <div class="item2">
-        {#if data.blogPosts && data.blogPosts.length > 0}
-            <ul>
-                {#each data.blogPosts as post}
-                    <li on:click={() => selectID(post.id)}>{post.title}</li>
-                {/each}
-            </ul>
-        {:else}
-            <h2>No blog posts available</h2>
-        {/if}
-    </div>
-    <div class="item3">
-         {#if data.blogPosts && data.blogPosts.length > 0}
-             {#each data.blogPosts as post}
-                {#if selectedID===post.id}
-                  {post.title}
-                  <Post {post} />
-                {/if}
-            {/each}
-        {:else}
-            <p>Select a post to view its details</p>
-        {/if}
-    </div>
-</div>
+{#if data.blogPosts && data.blogPosts.length > 0}
+	{#each data.blogPosts as post}
+		<Post {post} />
+	{/each}
+{:else}
+	<h2>No blog posts available</h2>
+{/if}
