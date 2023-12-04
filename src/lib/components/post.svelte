b7a5ee90c4be51348a22dd20177d489c6dc9b18c
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-04 09:42:21 +0530

    Update post.svelte

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -32,80 +32,29 @@
 </script>
 
 
-<style>
-    details {
-        background-color: #fff;
-        border: 1px solid #ddd;
-        border-radius: 8px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        padding: 15px;
-        margin-bottom: 20px;
-        transition: box-shadow 0.3s ease;
-    }
-
-    details:hover {
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
-    }
-
-    summary {
-        font-weight: bold;
-        cursor: pointer;
-        padding: 10px 0;
-        outline: none;
-    }
-
-    .secondary {
-        color: #333;
-    }
-
-    .contrast {
-        background-color: #f0f0f0;
-        padding: 2px 5px;
-        border-radius: 3px;
-    }
-
-    .content {
-        padding-top: 10px;
-    }
-
-    figure {
-        margin: 0;
-    }
-
-    img {
-        width: 100%;
-        height: auto;
-        border-radius: 5px;
-    }
-
-    .yellow-banner {
-        background-color: #ffcc00;
-        padding: 5px;
-        border-radius: 0 0 5px 5px;
-        text-align: center;
-    }
-</style>
-
 <details>
-    <summary role="button" class="secondary">
-        <span role="button" class="contrast">{post.id} </span>
-        {'   ' + post.created_at.toLocaleDateString('en-GB', {
-            year: '2-digit',
-            month: 'short',
-            day: '2-digit'
-        }) + '-' + post.title}
-    </summary>
+	<!-- svelte-ignore a11y-no-redundant-roles -->
+	<summary role="button" class="secondary"
+		><span role="button" class="contrast">{post.id} </span>{'   ' +
+			post.created_at.toLocaleDateString('en-GB', {
+				year: '2-digit',
+				month: 'short',
+				day: '2-digit'
+			}) +
+			'-' +
+			post.title}</summary
+	>
 
-    <article class="content">
-        <figure>
-            <img src={post.image_url} alt="pic" />
-            <figcaption class="yellow-banner">{post.title}</figcaption>
-        </figure>
-    </article>
+	<article class="content">
+		<figure>
+			<img src={post.image_url} alt="pic" />
 
-    <article class="content">
-        {@html newBlogContent}
-    </article>
+			<figcaption class="yellow-banner">
+				{post.title}
+			</figcaption>
+		</figure>
+	</article>
+	<article class="content">
+		{@html newBlogContent}
+	</article>
 </details>
-
-
