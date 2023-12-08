3a053af2317c80df2499d21aac06f9e440f42bea
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-08 06:02:10 +0530

    Add export statement for prerender variable

--- src/routes/blogs/+page.server.js
+++ src/routes/blogs/+page.server.js
@@ -1,6 +1,6 @@
 import { createPool } from '@vercel/postgres';
 import { POSTGRES_URL } from '$env/static/private';
-
+export const prerender = false;
 export async function load({}) {
 	const pool = createPool({
 		connectionString: POSTGRES_URL
