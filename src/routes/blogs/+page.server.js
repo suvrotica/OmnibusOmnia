8e8519037d37c5988a93f2339df4de56a60ae85e
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 01:39:21 +0530

    Update +page.server.js

--- src/routes/blogs/+page.server.js
+++ src/routes/blogs/+page.server.js
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 export const prerender = false;
 export async function load({}) {
-	const { rows } = await sql`SELECT * FROM blog_posts;`;
+	const { rows } = await sql`select id,title,tag_set,created_at from blog_posts order by created_at desc;`;
 	return { blogPosts: rows };
 }
