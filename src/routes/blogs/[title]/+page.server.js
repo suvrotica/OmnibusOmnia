2420c7a0b4a74bd7833eb0288824fedcb19b2894
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 02:53:11 +0530

    Update +page.server.js

--- src/routes/blogs/[title]/+page.server.js
+++ src/routes/blogs/[title]/+page.server.js
@@ -1,6 +1,6 @@
 export async function load({ params }) {
         // Extract the title from the URL
-        const { title } = params.slug;
+        const { title } = params;
         
         return title;
 }
