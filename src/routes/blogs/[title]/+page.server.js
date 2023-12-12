244318d3f64e701fc5697c90c16c5c85128f9277
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 05:19:32 +0530

    Update +page.server.js

--- src/routes/blogs/[title]/+page.server.js
+++ src/routes/blogs/[title]/+page.server.js
@@ -4,6 +4,6 @@ export const prerender = false;
 
 export async function load({ params }) {
         const title=params.title;
-        const { rows } = await sql`SELECT id,title,tag_set,image_url,created_at FROM blog_posts WHERE TRIM(title)=${title};`;
+        const { rows } = await sql`SELECT id,title,content,tag_set,image_url,created_at FROM blog_posts WHERE TRIM(title)=${title};`;
         return {post:rows[0]};
 }
