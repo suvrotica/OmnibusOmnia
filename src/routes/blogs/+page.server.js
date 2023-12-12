c78d55e3ef258bc522df142873762660ba2754ee
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 01:31:22 +0530

    Update +page.server.js

--- src/routes/blogs/+page.server.js
+++ src/routes/blogs/+page.server.js
@@ -1,10 +1,6 @@
-import { createPool } from '@vercel/postgres';
-import { POSTGRES_URL } from '$env/static/private';
+import { sql } from '@vercel/postgres';
 export const prerender = false;
 export async function load({}) {
-	const pool = createPool({
-		connectionString: POSTGRES_URL
-	});
-	const { rows } = await pool.sql`SELECT * FROM blog_posts ORDER BY title;`;
+	const { rows } = await sql`SELECT * FROM blog_posts;`;
 	return { blogPosts: rows };
 }
