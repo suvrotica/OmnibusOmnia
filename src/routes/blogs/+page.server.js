e0af3593e53748987aacb53ab5bd97ddbaf3e339
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 02:07:32 +0530

    Delete src/routes/blogs/+page.server.js

--- src/routes/blogs/+page.server.js
+++
@@ -1,6 +0,0 @@
-import { sql } from '@vercel/postgres';
-export const prerender = false;
-export async function load({}) {
-	const { rows } = await sql`select id,title,tag_set,image_url,created_at from blog_posts order by created_at desc;`;
-	return { blogPosts: rows };
-}
