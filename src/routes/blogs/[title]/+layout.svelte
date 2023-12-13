60971df2e373276b4474939df8effaa8a91006cc
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 11:17:06 +0530

    Update +layout.svelte

--- src/routes/blogs/[title]/+layout.svelte
+++ src/routes/blogs/[title]/+layout.svelte
@@ -4,9 +4,9 @@
 <main class="grid">
    <slot/>
 
-   <aside data-sveltekit-reload>
+   <aside >
       {#each data.blogPosts as post(post.id)}
-        <li><a href="/blogs/{post.title}">{post.title}</a></li>
+        <li data-sveltekit-reload><a href="/blogs/{post.title}">{post.title}</a></li>
       {/each}
    </aside>
 </main>
