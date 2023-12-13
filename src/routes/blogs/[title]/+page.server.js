3761a30f6a85c39625579846c3d5aa574ade3919
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 11:08:46 +0530

    Update +page.server.js

--- src/routes/blogs/[title]/+page.server.js
+++ src/routes/blogs/[title]/+page.server.js
@@ -1,10 +1,9 @@
-//import { sql } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
 
 export const prerender = false;
 
 export async function load({ params }) {
         const title = params.title;
-        //const { rows } = await sql`SELECT id,title,content,tag_set,image_url,created_at FROM blog_posts WHERE TRIM(title)=${title};`;
-        //return {post:rows[0]};
-        return {title};
+        const { rows } = await sql`SELECT id,title,content,tag_set,image_url,created_at FROM blog_posts WHERE TRIM(title)=${title};`;
+        return {post:rows[0]};
 }
