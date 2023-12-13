3f7426d9fa6411a1fd71827d3885bdfde182ed1a
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 10:52:38 +0530

    Update +page.svelte

--- src/routes/blogs/[title]/+page.svelte
+++ src/routes/blogs/[title]/+page.svelte
@@ -1,11 +1,12 @@
 <script>
-import Post from '$lib/components/post.svelte';
+//import Post from '$lib/components/post.svelte';
 export let data;
-const post=data.post;
+//const post=data.post;
+const title = data.title;
 </script>
-
-{#if !post}
- <h1>No post with that title</h1>
-{:else}
- <Post {post}/>
-{/if}
+<h1>{title}</h1>
+//{#if !post}
+// <h1>No post with that title</h1>
+//{:else}
+ //<Post {post}/>
+//{/if}
