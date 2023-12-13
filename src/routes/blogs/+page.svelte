a8a0be676a2df033f93e5d628f7c329905bc675e
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 14:03:12 +0530

    Update +page.svelte

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -2,6 +2,15 @@
 	export let data;
 	import Gallery from '$lib/components/gallery.svelte';
 
+        function createSlug(title) {
+          return title
+            .toLowerCase() // Convert to lowercase
+            .trim() // Remove whitespace from both ends of a string
+            .replace(/\s+/g, '-') // Replace spaces with hyphens
+            .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+            .replace(/\-\-+/g, '-'); // Replace multiple hyphens with single hyphen
+        }
+
 	function groupPostsByTag(posts) {
 		const grouped = {};
 		posts.forEach((post) => {
@@ -18,13 +27,15 @@
 		});
 		return grouped;
 	}
-
+        
 	const groupedPosts = groupPostsByTag(data.blogPosts);
 </script>
+
 {#each data.blogPosts as post(post.id)}
-<p>{post.id}</p>
-<li><a href="/blogs/{post.title}">{post.title}</a></li>
+   <p>{post.id}</p>
+   <li><a href="/blogs/{createSlug(post.title)}">{post.title}</a></li>
 {/each}
+
 {#if Object.keys(groupedPosts).length > 0}
 	{#each Object.keys(groupedPosts) as tag}
 		<Gallery {tag} {groupedPosts} />
