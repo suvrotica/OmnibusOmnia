52dbb435731b4db1a0ba56a23008fa816ec69566
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 05:53:27 +0530

    Update +layout.svelte

--- src/routes/blogs/[title]/+layout.svelte
+++ src/routes/blogs/[title]/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
    export let data;
 </script>
-<slot/>
+<main>
+   <slot/>
+</main>
 <aside>
    {#each data.blogPosts as post(post.id)}
         <li><a href="/blogs/{post.title}">{post.title}</a></li>
