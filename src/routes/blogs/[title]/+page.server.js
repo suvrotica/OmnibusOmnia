4ee13004419d79757313476a0a9da1b451395728
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 14:30:09 +0530

    Delete src/routes/blogs/[title] directory

--- src/routes/blogs/[title]/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { createSlug } from '$lib/utility-functions';
-export const prerender = false;
-
-export async function load({ params }) {
-        const title = params.title;
-        const { rows } = await sql`SELECT id,title,content,tag_set,image_url,created_at FROM blog_posts WHERE TRIM(title)=${title};`;
-        return {post:rows[0]};
-}
