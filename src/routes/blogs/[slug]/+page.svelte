b94a9e654c9cac73de9decd2f629f57b1a55c763
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 14:29:48 +0530

    Create +page.svelte

---
+++ src/routes/blogs/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Post from '$lib/components/post.svelte';
+  export const data;
+  const post = data.post;
+</script>
+
+{#if !post}
+   <h1>No post with that title</h1>
+{:else}
+   <p>{post.title}</p>
+   <Post {post}/>
+{/if}
