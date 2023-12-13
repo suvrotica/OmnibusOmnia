30d1f6987a7cc171dc813951578d13ec62d95761
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 14:42:38 +0530

    Update +page.svelte

--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -2,8 +2,15 @@
   import Post from '$lib/components/post.svelte';
   export let data;
   const post = data.post;
+  const posts = data.posts;
 </script>
 
-
-<Post {post}/>
+<main class="grid">
+   <Post {post}/>
+   <aside>
+     {#each posts as post}
+       {post.title}
+     {/each}
+   </aside>
+</main>
 
