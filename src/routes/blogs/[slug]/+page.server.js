82c89f822a61afb53a9daa631dc2b43f69d4c5fe
suvrotica <117360356+suvrotica@users.noreply.github.com>
2023-12-13 15:07:46 +0530

    Update +page.server.js

--- src/routes/blogs/[slug]/+page.server.js
+++ src/routes/blogs/[slug]/+page.server.js
@@ -7,8 +7,9 @@ export async function load({ params }) {
     const slug = params.slug;
 
     // Fetch all post meta data but not content
-    const metaDataRows = await sql`SELECT id, title, tag_set FROM blog_posts;`;
-
+    const {rows}  = await sql`SELECT id, title, tag_set FROM blog_posts;`;
+    const metaDataRows = rows;
+    
     // Find the post that matches the slug
     const metaDataPost = metaDataRows.find(row => createSlug(row.title) === slug);
 
@@ -16,8 +17,8 @@ export async function load({ params }) {
     const id = metaDataPost.id;
 
     // get the post whose title matched slug
-    const post = await sql`SELECT * FROM blog_posts WHERE id=${id};`;
-
+    const {rows} = await sql`SELECT * FROM blog_posts WHERE id=${id};`;
+    const post=rows[0];
 
     if (post) {
         return { metaDataRows, post };
