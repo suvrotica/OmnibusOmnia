904ce616bded3b994acafb7b1d7ca3b2c6abed4e
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-12-20 10:33:10 +0530

    Add dotenv package and update gallery component

--- src/routes/blogs/+page.server.js
+++ src/routes/blogs/+page.server.js
@@ -1,4 +1,5 @@
 //import { sql } from '@vercel/postgres';
+import 'dotenv/config';
 import { createPool } from '@vercel/postgres';
 import { POSTGRES_URL } from '$env/static/private';
 const pool = createPool({
@@ -6,6 +7,7 @@ const pool = createPool({
 });
 export const prerender = false;
 export async function load({}) {
-	const { rows } = await pool.sql`SELECT id,title,tag_set,image_url,created_at from blog_posts order by created_at desc;`;
+	const { rows } =
+		await pool.sql`SELECT id,title,tag_set,image_url,created_at from blog_posts order by created_at desc;`;
 	return { blogPosts: rows };
 }
--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -2,14 +2,14 @@
 	export let data;
 	import Gallery from '$lib/components/gallery.svelte';
 
-        function createSlug(title) {
-          return title
-            .toLowerCase() // Convert to lowercase
-            .trim() // Remove whitespace from both ends of a string
-            .replace(/\s+/g, '-') // Replace spaces with hyphens
-            .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-            .replace(/\-\-+/g, '-'); // Replace multiple hyphens with single hyphen
-        }
+	function createSlug(title) {
+		return title
+			.toLowerCase() // Convert to lowercase
+			.trim() // Remove whitespace from both ends of a string
+			.replace(/\s+/g, '-') // Replace spaces with hyphens
+			.replace(/[^\w\-]+/g, '') // Remove all non-word chars
+			.replace(/\-\-+/g, '-'); // Replace multiple hyphens with single hyphen
+	}
 
 	function groupPostsByTag(posts) {
 		const grouped = {};
@@ -27,14 +27,15 @@
 		});
 		return grouped;
 	}
-        
+
 	const groupedPosts = groupPostsByTag(data.blogPosts);
 </script>
 
+<!-- 
 {#each data.blogPosts as post(post.id)}
    <p>{post.id}</p>
    <li><a href="/blogs/{createSlug(post.title)}">{post.title}</a></li>
-{/each}
+{/each} -->
 
 {#if Object.keys(groupedPosts).length > 0}
 	{#each Object.keys(groupedPosts) as tag}
