7c424fc99c258789ad7d04cf09793e7090af7247
suvrotica <117360356+suvrotica@users.noreply.github.com>
2024-01-05 15:20:25 +0530

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -46,6 +46,59 @@
         </li>
     </ul>
 </nav>
+<style>
+    a, ul, li {
+        color: rgb(10, 10, 10);
+        background-color: white;
+        padding: 0px;
+        margin: 0px;
+    }
+
+    i {
+        padding: 10px;
+        margin: 10px;
+    }
+
+    .logo {
+        width: 600px;
+    }
+
+    nav {
+        margin: 0px;
+        padding: 0px;
+        background: linear-gradient(to right, black, white 30%);
+    }
+
+    /* Style for the icon in the active link */
+    a[aria-current="page"] i {
+        color: #ff4500; /* Change the icon color */
+        /* Additional styles can be added here */
+    }
+
+    /* Spinner CSS */
+    .spinner {
+        border: 4px solid rgba(0, 0, 0, 0.1);
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        border-left-color: #09f;
+        animation: spin 1s ease infinite;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
+
 
 <style>
     /* Your existing styles */
